ca9ecda51623fa8fc3de30a2fe20cec76dc9baa8
Michael Ostermann <michaelostermann@me.com>
2025-12-03 12:51:28 +0100

    feat: improve syncModalGroupsToBody

--- packages/signals-modal/src/utils/syncModalGroupsToBody.ts
+++ packages/signals-modal/src/utils/syncModalGroupsToBody.ts
@@ -1,19 +1,24 @@
-import { effect, INTERNAL } from "@monstermann/signals"
+import { effect, INTERNAL, memo } from "@monstermann/signals"
 import { getGroupsForModal } from "../groups/getGroupsForModal"
 import { getVisibleModals } from "../status/getVisibleModals"
 
+const $classes = memo(() => {
+    const modals = getVisibleModals()
+    const clx = new Set<string>()
+    for (const modal of modals) {
+        for (const group of getGroupsForModal(modal)) {
+            clx.add(`has-${group}`)
+        }
+    }
+    return Array.from(clx).sort().join(" ")
+}, INTERNAL)
+
 export function syncModalGroupsToBody(): void {
     effect(() => {
-        const modals = getVisibleModals()
-        const clx = new Set<string>()
-        for (const modal of modals) {
-            for (const group of getGroupsForModal(modal)) {
-                clx.add(`has-${group}`)
-            }
-        }
-        document.body.classList.add(...clx)
+        const classes = $classes().split(" ")
+        document.body.classList.add(...classes)
         return () => {
-            document.body.classList.remove(...clx)
+            document.body.classList.remove(...classes)
         }
     }, INTERNAL)
 }
